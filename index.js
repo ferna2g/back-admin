9df58f44fba0a3cb6d5936dc6fcc2491320d893e
fer g <arielgeronimo517@gmail.com>
2021-01-16 17:53:54 -0600

    creando el servidor de express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('desde index.js');
+const express = require('express');
+
+//crear el servidor
+const app = express();
+
+//puerto de la app
+const PORT = process.env.PORT || 4000;
+
+//definir la pagina principal
+//app.get('/', (req, res) => {
+  //res.send('Hola Mundo')
+//})
+
+//start app
+app.listen(PORT, () => {
+  console.log(`el servidor esta funcionando en el puerto ${PORT}`);
+})
