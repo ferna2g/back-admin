b920168384a1c619b82e99572bbaf3339fa3d65f
fer g <arielgeronimo517@gmail.com>
2021-01-17 13:05:15 -0600

    eliminar un proyecto

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -80,3 +80,31 @@ exports.actualizarProyecto = async (req, res) => {
     res.status(500).send('Error en el servidor')
   }
 }
+
+
+//elimina un proyecto por su id
+exports.eliminarProyecto = async (req, res ) => {
+
+  try {
+    //revisar el ID
+    let proyecto = await Proyecto.findById(req.params.id);
+
+    //si el proyecto existe o no
+    if (!proyecto) {
+      return res.status(404).json({msg: 'Proyecto no encontrado'})
+    }
+
+    //verificar el creador del proyecto
+    if (proyecto.creador.toString() !== req.usuario.id ) {
+      return res.status(401).json({msg:'No autorizado'})
+    }
+
+    //elimminar el proyecto
+    await Proyecto.findOneAndRemove({ _id: req.params.id });
+    res.json({ msg: 'Proyecto eliminado'})
+
+  } catch (e) {
+    console.log(e);
+    res.status(500).send('Error en el servidor')
+  }
+}
