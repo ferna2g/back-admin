cc9ea16554f346945022403f43822f96d976406c
fer g <arielgeronimo517@gmail.com>
2021-01-17 10:33:11 -0600

    creando routing y controllers para los proyectos

---
+++ routes/proyectos.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const proyectoController = require('../controllers/proyectoController');
+
+//crea proyectos
+// api/proyectos
+router.post('/', proyectoController.crearProyecto)
+
+module.exports = router;
