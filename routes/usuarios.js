5b7692ea6949bcc46f021bb44dad053e42853c5d
fer g <arielgeronimo517@gmail.com>
2021-01-16 20:28:17 -0600

    configurando las rutas para los usuarios

---
+++ routes/usuarios.js
@@ -0,0 +1,11 @@
+//rutas para crear usuarios
+const express = require('express');
+const router = express.Router();
+
+//crea un uusario
+//api/usuarios
+router.post('/', () => {
+
+});
+
+module.exports = router;
