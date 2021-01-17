ebe3c079b2d35b154b9f49b942073683cfd46849
fer g <arielgeronimo517@gmail.com>
2021-01-17 13:44:43 -0600

    obtener las tareas por cada proyecto

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -13,7 +13,9 @@ exports.crearTarea = async (req, res) => {
 
   //extraer el proyecto y comprobar is existe
   const { proyecto } = req.body;
+
   try {
+
     const existeProyecto = await Proyecto.findById(proyecto);
     if (!existeProyecto) {
       return res.status(404).json({msg: 'Proyecto no encontrado'})
@@ -28,6 +30,33 @@ exports.crearTarea = async (req, res) => {
     const tarea = new Tarea(req.body);
     await tarea.save();
     res.json({ tarea });
+
+  } catch (e) {
+    console.log(e);
+    res.status(500).send('Hubo un error')
+  }
+}
+
+//obtiene las tareas por proeyecto
+exports.obtenerTareas = async (req, res) => {
+
+  try {
+    //extraer el proyecto y comprobar si existe
+    const { proyecto } = req.body;
+
+    const existeProyecto = await Proyecto.findById(proyecto);
+    if (!existeProyecto) {
+      return res.status(404).json({msg: 'Proyecto no encontrado'})
+    }
+
+    //Revisar si el proyecto acutal pertenece al usuario autenticado
+    if (existeProyecto.creador.toString() !== req.usuario.id ) {
+        return res.status(401).json({msg: 'No autorizado'});
+    }
+
+    //obtener tareas por proyecto
+    const tareas = await Tarea.find({ proyecto  });
+    res.json(tareas)
 
   } catch (e) {
     console.log(e);
