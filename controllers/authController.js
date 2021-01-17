417da81e5a6e5422a8766d020a9971e8409702fb
fer g <arielgeronimo517@gmail.com>
2021-01-17 10:19:24 -0600

    validando si el usuario existe o no, si el password es correcto o incorrecto

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,5 +11,40 @@ exports.autenticarUsuario = async (req, res) => {
     return res.status(400).json({errores: errores.array() })
   }
 
+  //extraer el email y password
+  const { email, password } = req.body;
 
+  try {
+    //revisar que sea un usuario registrado
+    let usuario = await Usuario.findOne({ email });
+    if(!usuario){
+      return res.status(400).json({msg: 'El usuario no existe'});
+    }
+
+    //revisar el password si es igual el ingresado con el de la base de datos
+    const passCorrecto = await bcryptjs.compare(password, usuario.password)
+    if (!passCorrecto) {
+      return res.status(400).json({msg: 'password incorrecto'})
+    }
+
+    //si todo es correcto crear y firmar el jwt
+    const payload = {
+      usuario: {
+        id: usuario.id
+      }
+    };
+
+    //firmar el jwt
+    jwt.sign(payload, process.env.SECRETA, {
+      expiresIn: 3600
+    }, (error, token) => {
+      if(error) throw error;
+
+      //mensaje de confirmacion
+      res.json({ token: token })
+    })
+
+  } catch (e) {
+    console.log(e);
+  }
 }
